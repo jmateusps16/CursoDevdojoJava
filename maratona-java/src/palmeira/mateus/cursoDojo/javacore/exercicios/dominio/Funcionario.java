afaa1274e2d76852a5863fb40aae83c87a4ee2c1
Mateus Palmeira <jmateusps16@gmail.com>
2022-10-16 01:10:18 -0300

    Refatorando o código, tornando escalavel

--- maratona-java/src/palmeira/mateus/cursoDojo/javacore/exercicios/dominio/Funcionario.java
+++ maratona-java/src/palmeira/mateus/cursoDojo/javacore/exercicios/dominio/Funcionario.java
@@ -1,38 +1,37 @@
 package palmeira.mateus.cursoDojo.javacore.exercicios.dominio;
 
 public class Funcionario extends Pessoa {
-	public double[] salario = new double[3];
+	public double[] salarios;
 
 	@Override
 	public void imprimeDados(Pessoa pessoa) {
 		System.out.println("Nome: " + this.nome);
 		System.out.println("Idade: " + this.idade);
+		imprimeSalarios();
+	}
 
-		if (this.salario[2] != 0) {
-			System.out.println("Ultimo salario: " + this.salario[2]);
-		} else if (this.salario[1] != 0) {
-			System.out.println("Ultimo salario: " + this.salario[1]);
-		} else if (this.salario[0] != 0){
-			System.out.println("Ultimo salario: " + this.salario[0]);
-		} else{
-			System.out.println("Não foi informado o salário");
+	private void imprimeSalarios() {
+		if (this.salarios == null) {
+			System.out.println("Salário não informado");
+		} else {
+			System.out.print("Ultimos Salarios: ");
+			for (double salario : salarios) {
+				System.out.print(salario + " R$, ");
+			}
+			System.out.println("");
 		}
 	}
 
 	public void imprimeMediaSalario(Funcionario funcionario) {
-		int qntSalario = 0;
-		double totalSalario = 0;
-		for(int i=0;i<this.salario.length;i++) {
-			if(this.salario[i]!=0) {
-				qntSalario++;
-			}
-			totalSalario += this.salario[i];
+		if (this.salarios == null) {
+			return;
 		}
-		if(qntSalario==0) {
-			System.out.println("Não foi informado o salário");
-		}else {
-			System.out.println("A média de salario é: "+totalSalario/qntSalario);
+		double media = 0;
+		for (double salario : salarios) {
+			media += salario;
 		}
+		media /= salarios.length;
+		System.out.println("Média dos salários: " + media);
 	}
 
 }
